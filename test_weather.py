2ff737039f9f9953b47e47671b5253f80607eeef
Bob Amand <biotmg@nc.rr.com>
2015-10-20 12:29:15 -0400

    added zip-code data mock test

--- test_weather.py
+++ test_weather.py
@@ -6,10 +6,19 @@ import os
 weather_key = os.environ['WKEY']
 #  'noqa' will allow override of long url flag in PEP8
 
+
+def test_geolookup():  # zip code data for applying to state + city
+    with requests_mock.mock() as m:
+        m.get('http://api.wunderground.com/api/WKEY/geolookup/q/94107.json',  # noqa
+              text='Successful test: Zip code mock data')
+        res = requests.get('http://api.wunderground.com/api/WKEY/geolookup/q/94107.json')  # noqa
+        print(res.text)
+
+
 def test_forecast10_day():  # 10 day forecast (at zip)
     with requests_mock.mock() as m:
         m.get('http://api.wunderground.com/api/WKEY/forecast10day/q/CA/San_Francisco.json',  # noqa
-              text='10 Day Forecast mock data: Successful test')
+              text='Successful test: 10 Day Forecast mock data')
         res = requests.get('http://api.wunderground.com/api/WKEY/forecast10day/q/CA/San_Francisco.json')  # noqa
         print(res.text)
 
@@ -17,7 +26,7 @@ def test_forecast10_day():  # 10 day forecast (at zip)
 def test_astronomy():      # sunrise + sunset (at zip)
     with requests_mock.mock() as m:
         m.get('http://api.wunderground.com/api/WKEY/astronomy/q/Australia/Sydney.json',  # noqa
-              text='Sunrise + Sunset mock data: Successful test')
+              text='Successful test: Sunrise + Sunset mock data')
         res = requests.get('http://api.wunderground.com/api/WKEY/astronomy/q/Australia/Sydney.json')  # noqa
         print(res.text)
 
@@ -25,7 +34,7 @@ def test_astronomy():      # sunrise + sunset (at zip)
 def test_alerts():          # alerts of any kind (at zip)
     with requests_mock.mock() as m:
         m.get('http://api.wunderground.com/api/WKEY/alerts/q/IA/Des_Moines.json',  # noqa
-              text='Alerts mock data: Successful test')
+              text='Successful test: Alerts mock data')
         res = requests.get('http://api.wunderground.com/api/WKEY/alerts/q/IA/Des_Moines.json')  # noqa
         print(res.text)
 
@@ -33,12 +42,13 @@ def test_alerts():          # alerts of any kind (at zip)
 def test_hurricane():       # hurricanes anywhere
     with requests_mock.mock() as m:
         m.get('http://api.wunderground.com/api/WKEY/currenthurricane/view.json',  # noqa
-              text='Hurricane mock data: Successful test')
+              text='Successful test: Hurricane mock data')
         res = requests.get('http://api.wunderground.com/api/WKEY/currenthurricane/view.json')  # noqa
         print(res.text)
 
 
 def test_run():
+    test_geolookup()
     test_forecast10_day()
     test_astronomy()
     test_alerts()
