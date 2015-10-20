f71d6b2fe900711253cdf5a26a04dd5802792e6e
Bob Amand <biotmg@nc.rr.com>
2015-10-19 23:52:36 -0400

    not working yet

--- weather.py
+++ weather.py
@@ -3,6 +3,11 @@ import os
 
 weather_key = os.environ['WKEY']
 
-res = request.get('http://......', header={'ddd': weather_key})
+res = requests.get('http://api.wunderground.com/api/WKEY/currenthurricane/view.json')
+# header={'ddd': weather_key})
 
 print(res.json())
+#
+#
+# current_location = input("Please enter your zip-code: ")
+# print("You entered: {}". format(current_location))
