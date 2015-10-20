9449edf63c7017aaed639edb2a75d586d4a1da08
Bob Amand <biotmg@nc.rr.com>
2015-10-20 00:35:49 -0400

    successful mock test with hurricane data

--- weather.py
+++ weather.py
@@ -1,12 +1,18 @@
 import requests
+import requests_mock
 import os
 
 weather_key = os.environ['WKEY']
 
-res = requests.get('http://api.wunderground.com/api/WKEY/currenthurricane/view.json')
+with requests_mock.mock() as m:
+    m.get('http://api.wunderground.com/api/WKEY/currenthurricane/view.json',
+          text='Successful test')
+    res = requests.get('http://api.wunderground.com/api/WKEY/currenthurricane/view.json')
+    print(res.text)
+
 # header={'ddd': weather_key})
 
-print(res.json())
+# print(res.json())
 #
 #
 # current_location = input("Please enter your zip-code: ")
